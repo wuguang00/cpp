76b846c55499ffe58d812e2ccc2b15e5e59f2601
wuguang <wuguang@wuguangs-MacBook-Pro.local>
2019-12-11 01:16:11 +0800

    add DMA case for simulation and fimiliarw

---
+++ basic/ch13/dma.h
@@ -0,0 +1,54 @@
+/**************************************************************************
+ File Name:     dma.h
+ Author:        wuguang
+ E-mail:        wuguang20084520@163.com
+ Created Time:  Wed Dec 11 00:04:35 2019
+ Release Notes: inheritance and dynamic memory allocation 
+ **************************************************************************/
+#ifndef DMA_H_
+#define DMA_H_
+#include <iostream>
+
+// Base Class Using DMA
+class baseDMA
+{
+    private:
+        char * label;
+        int rating;
+    public:
+        baseDMA(const char * l = "null", int r = 0);
+        baseDMA(const baseDMA & rs);
+        virtual ~baseDMA();
+        baseDMA & operator=(const baseDMA & rs);
+        friend std::ostream & operator<<(std::ostream & os, const baseDMA & rs);
+};
+
+// derived class without DMA
+// no destructor needed
+// uses implicit copy constructor
+// uses implicit assignment operator
+class lacksDMA : public baseDMA
+{
+    private:
+        enum {COL_LEN = 40}; // define a enum
+        char color[COL_LEN];
+    public:
+        lacksDMA(const char * c = "blank", const char * l = "null", int r = 0);
+        lacksDMA(const char * c, const baseDMA & rs);
+        friend std::ostream & operator<<(std::ostream & os, const lacksDMA & rs);
+};
+
+// derived class with DMA
+class hasDMA : public baseDMA
+{
+    private:
+        char * style;
+    public:
+        hasDMA(const char * s = "none", const char * l = "null", int r = 0);
+        hasDMA(const char * s, const baseDMA & rs);
+        hasDMA(const hasDMA & hs);
+        ~hasDMA();
+        hasDMA & operator=(const hasDMA & rs);
+        friend std::ostream & operator<<(std::ostream & os, const hasDMA & rs);
+};
+#endif
---
+++ basic/ch13/usedma.cpp
@@ -0,0 +1,35 @@
+/**************************************************************************
+ File Name:     usedma.cpp
+ Author:        wuguang
+ E-mail:        wuguang20084520@163.com
+ Created Time:  Wed Dec 11 01:01:08 2019
+ Release Notes: inheritance, friends, and DMA 
+ **************************************************************************/
+#include <iostream>
+#include "dma.h"
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+
+    baseDMA shirt("Portabelly", 8);
+    lacksDMA balloon("red", "Blimpo", 4);
+    hasDMA map("Mercator", "Buffalo Keys", 5);
+    cout << "Displaying baseDMA object:\n";
+    cout << shirt << endl;
+    cout << "Displaying lacksDMA object:\n";
+    cout << balloon << endl;
+    cout << "Displaying hasDMA object:\n";
+    cout << map << endl;
+
+    lacksDMA balloon2(balloon);
+    cout << "Result of lacksDMA copy:\n";
+    cout << balloon2 << endl;
+    hasDMA map2;
+    map2 = map;
+    cout << "Result of hasDMA assignment:\n";
+    cout << map2 << endl;
+
+    return 0;
+}
